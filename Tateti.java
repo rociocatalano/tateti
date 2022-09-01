33707ec6dd589cad1cb0f9a7f8d0b95a0f01cad8
Rocío Catalano <rociocatalano4@hotmail.com>
2022-08-31 21:33:43 -0300

    First version

---
+++ Tateti.java
@@ -0,0 +1,87 @@
+package Tateti;
+
+public class Tateti {
+	
+	java.util.Scanner lector = new java.util.Scanner(System.in);
+	
+	private static char[][] tablero;
+	boolean jugadaCorrecta = false;
+	boolean hayGanador = false;
+	int jugadasCompletas = 0;
+	
+	void crearYLlenarTablero() {
+		tablero = new char[3][3];
+		for (int i=0; i < tablero.length; i++) {
+			for (int j=0; j < tablero[i].length; j++) {
+				tablero[i][j] = '-';
+			}
+		}
+	}
+	
+	void mostrarTablero() {
+		for (int i=0; i < tablero.length; i++) {
+			for (int j=0; j < tablero[i].length; j++) {
+				System.out.printf("|%c", tablero[i][j]);
+			}
+			System.out.println("|");
+			System.out.println();
+		}
+	}
+	
+	void movimientoJugador() {
+		System.out.println("Por favor ingrese su jugada");
+		while (!jugadaCorrecta) {
+			System.out.println("Ingrese la fila elegida:");
+			int filaJugador = Integer.parseInt(lector.nextLine());
+			System.out.println("Ingrese la columna elegida:");
+			int columnaJugador = Integer.parseInt(lector.nextLine());
+			if (tablero[filaJugador][columnaJugador] == '-') {		
+				tablero[filaJugador][columnaJugador] = 'X';
+				jugadaCorrecta = true;
+				jugadasCompletas++;
+				mostrarTablero();
+			} else {
+				System.out.println("Esa posición ya se encuentra ocupada,\npor favor intente de nuevo.");
+				mostrarTablero();
+			}
+		}		
+	}
+	
+	void movimientoComputadora() {
+		boolean completo = false;
+		
+		System.out.println("Jugada del adversario:");
+		
+		while (!completo) {
+			int fila = (int)(Math.random() * (2-0+1));
+			int columna = (int)(Math.random() * (2-0+1));
+			if (tablero[fila][columna] == '-') {
+				tablero[fila][columna] = 'O';
+				completo = true;
+				jugadasCompletas++;
+				mostrarTablero();
+			} else {
+				System.out.println("no se pudo");
+			}
+		}		
+		
+ 	}
+	
+	void comprobarGanador(char ficha) {
+		for (int i =0; i<3; i++) {
+			//comprobando columnas
+			if (tablero[0][i] == ficha && tablero[1][i] == ficha && tablero[2][i] == ficha) {
+				hayGanador = true;
+				//comprobando filas
+			} else if (tablero[i][0] == ficha && tablero[i][1] == ficha && tablero[i][2] == ficha) {
+				hayGanador = true;
+				//comprobando las diagonales por separado
+			} else if (tablero[0][0] == ficha && tablero[1][1] == ficha && tablero[2][2] == ficha) {
+				hayGanador = true;
+			} else if (tablero[2][0] == ficha && tablero[1][1] == ficha && tablero[0][2] == ficha) {
+				hayGanador = true;			
+			}
+		}
+	}
+	
+}
---
+++ pruebaTateti.java
@@ -0,0 +1,36 @@
+package Tateti;
+
+public class pruebaTateti {
+
+	public static void main(String[] args) {
+		Tateti jugar = new Tateti();
+		System.out.println("Bienvenido al Tateti :)");
+		int jugador = 1;
+		jugar.crearYLlenarTablero();
+		jugar.mostrarTablero();
+		while (!jugar.hayGanador && jugar.jugadasCompletas < 9) {
+			if (jugador == 1) {
+				jugar.movimientoJugador();
+				jugar.comprobarGanador('X');
+				jugar.jugadaCorrecta = false;
+				jugador = 2;
+			} else {
+				jugar.movimientoComputadora();
+				jugar.comprobarGanador('O');
+				jugar.jugadaCorrecta = false;
+				jugador = 1;
+			}
+		}
+
+		if (jugar.hayGanador && jugador == 2) {
+			System.out.println("¡Felicitaciones! Usted es el ganador.");
+		} else if (jugar.hayGanador && jugador == 1) {
+			System.out.println("Su adversario ganó. Intente nuevamente.");
+		} else if (jugar.jugadasCompletas == 9 && !jugar.hayGanador) {
+			System.out.println("Fin del juego. Hubo un empate. \nLo invitamos a intentar de nuevo");
+		}
+		
+
+	}
+
+}
